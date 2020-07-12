0a57ace1dd9f83cefd5053367c054ec41bd54616
Stewart C. Russell <scruss@scruss.com>
2020-07-12 09:49:42 -0400

    corrected, or at least explained, calibration

--- temperature.py
+++ temperature.py
@@ -1,6 +1,6 @@
-# hardware platform: Brainpad
+# hardware platform: Brainpad Classic/BP2
 # read the temperature value with the onboard mcp9701a sensor
-# might be reading a bit high
+# not super accurate: I see Vs around 3.15 V, so indicates ~+2 °C at room temp for me
 # scruss, 2020-06
 
 from pyb import ADC,Pin
@@ -8,7 +8,7 @@ from time import sleep
 adc0 = ADC(Pin('B0'))
 
 while True:
-    volts = 3.3 * adc0.read() / 4096
-    degC = (volts - 0.427) / 0.0195
+    volts = 3.3 * adc0.read() / 4095
+    degC = (volts - 0.4) / 0.019
     print('%7.1f degC (%7.3f V)' % (degC, volts))
     sleep(0.5)
