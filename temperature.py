0c8fa7f6e734c454e67b12af24c86c450e9b0357
Stewart C. Russell <scruss@scruss.com>
2020-07-12 21:56:41 -0400

    now uses internal vref and core_temp values

--- temperature.py
+++ temperature.py
@@ -1,14 +1,18 @@
-# hardware platform: Brainpad Classic/BP2
-# read the temperature value with the onboard mcp9701a sensor
-# not super accurate: I see Vs around 3.15 V, so indicates ~+2 °C at room temp for me
-# scruss, 2020-06
+# -*- coding: utf-8 -*-
+# brainpad bp2 temperature sensor vs core temperature
+# scruss, 2020-07
 
-from pyb import ADC,Pin
+
+from pyb import ADC, Pin, ADCAll
 from time import sleep
-adc0 = ADC(Pin('B0'))
+from math import log
+
+tmp = ADC(Pin('B0'))            # MCP9701A on PB0
+adc = ADCAll(12, 0x70000)       # enable internal sensors
 
 while True:
-    volts = 3.3 * adc0.read() / 4095
-    degC = (volts - 0.4) / 0.019
-    print('%7.1f degC (%7.3f V)' % (degC, volts))
-    sleep(0.5)
+    vref = adc.read_vref()
+    degC = ((vref * tmp.read() / 4095) - 0.4) / 0.01953
+    print('mcu: %5.1f degC :: sensor: %5.1f degC' %
+          (adc.read_core_temp(), degC))
+    sleep(1.2)
